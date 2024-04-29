44b7440eacfd585aa924a996152beab05241560a
Danilo Sena Santos <98361804+SenaNilo@users.noreply.github.com>
2024-04-29 16:05:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function circularArrayRotation(a, k, queries) {
     var aux = [];
     var lastValue = 0;
     for(let i = 0; i < k; i++){
-        lastValue = a.pop();
-        a.unshift(lastValue);
+        lastValue = a.pop();//Pega o ultimo valor da array
+        a.unshift(lastValue);//Coloca um valor no começo da array
     }
     for(let j = 0; j < queries.length; j++){
         aux.push(a[queries[j]]);
